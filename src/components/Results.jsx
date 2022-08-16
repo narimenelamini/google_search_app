d3d0ecd6749d92939a585e9286a939a9182fa963
Narimene Lamini <lamininarimene123@gmail.com>
2022-08-16 21:10:17 +0100

    Adding the vodeos section

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -25,7 +25,7 @@ export const Results = () => {
       case '/search':
         return (
           <div className="sm:px-56 flex flex-wrap justify-between space-y-6">
-            {results?.results?.map(({ link, title }, index) => (
+            {results?.map(({ link, title }, index) => (
               <div key={index} className="md:w-2/5 w-full">
                 <a href={link} target="_blank" rel="noreferrer">
                   <p className="text-sm">{link.length > 30 ? link.substring(0, 30) : link}</p>
@@ -38,7 +38,7 @@ export const Results = () => {
       case '/images':
         return (
           <div className="flex flex-wrap justify-center items-center">
-            {results?.image_results?.map(({ image, link: { href, title } }, index) => (
+            {results?.map(({ image, link: { href, title } }, index) => (
               <a href={href} target="_blank" key={index} rel="noreferrer" className="sm:p-3 p-5">
                 <img src={image?.src} alt={title} />
                 <p className="sm:w-36 w-36 break-words text-sm mt-2">{title}</p>
@@ -47,10 +47,30 @@ export const Results = () => {
           </div>
         );
       case '/news':
-             return 'SEARCH';
+        return (
+          <div className="sm:px-56 flex flex-wrap justify-between items-center space-y-6">
+            {results?.map(({ id, links, source, title }) => (
+              <div key={id} className="md:w-2/5 w-full ">
+                <a href={links?.[0].href} target="_blank" rel="noreferrer " className="hover:underline ">
+                  <p className="text-lg text-blue-700">{title}</p>
+                </a>
+                <div className="flex gap-4">
+                  <a href={source?.href} target="_blank" rel="noreferrer" className="hover:underline hover:text-blue-300"> {source?.href}</a>
+                </div>
+              </div>
+            ))}
+          </div>
+        );
       case '/videos':
-             return 'SEARCH';
-    
+        return (
+          <div className="flex flex-wrap ">
+            {results?.map((video, index) => (
+              <div key={index} className="p-30">
+                <ReactPlayer url={video.additional_links?.[0].href} controls width="355px" height="200px" />
+              </div>
+            ))}
+          </div>
+        );
       default:
          return 'ERROR';
         break;
--- src/context/ResultContextProvider.js
+++ src/context/ResultContextProvider.js
@@ -23,8 +23,16 @@ export const ResultContextProvider = ({ children }) => {
             }
              });
              const data = await response.json();
-             console.log(data);
-             setResults(data);
+             console.log({type, data});
+
+             if(type.includes('/news')){
+                  setResults( data.entries);
+
+             } else  if(type.includes('/images')){
+                  setResults( data.image_results);
+
+             } else setResults (data.results);
+             
              setIsLoding(false);
              
        }
