be2426dc054d74a3cb995265943a342412c9dd2d
Narimene Lamini <lamininarimene123@gmail.com>
2022-08-16 21:37:18 +0100

    Setting the Navbar links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const Footer = () => (
   <div className="text-center p-10 mt-10 border-t border-gray-200 ">
-    © 2021 Google, Inc.
+    © 2022 Google, Inc.
   </div>
 );
